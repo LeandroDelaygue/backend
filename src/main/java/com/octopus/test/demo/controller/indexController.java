3aa6f51a432ce9a0b98d6bae43662532a5fd8be1
unknown <ldelaygue@gmail.com>
2019-03-02 09:41:28 -0300

    first commit

---
+++ src/main/java/com/octopus/test/demo/controller/indexController.java
@@ -0,0 +1,13 @@
+package com.octopus.test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+	@RequestMapping("/")
+	public String getIndexPage() {
+		return "index";
+	}
+}
